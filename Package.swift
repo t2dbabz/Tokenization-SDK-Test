1c6a63b1309cb37fd609b369a653c03efc215515
Babatunde Owoleke <tundeowoleke1@gmail.com>
2026-05-05 17:29:42 +0100

    Update checksum for CardTokenizationSdk

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "CardTokenizationSdk",
             url: "https://github.com/t2dbabz/Tokenization-SDK-Test/releases/download/v1.0.1/CardTokenizationSdkCoreKit.xcframework.zip",
-            checksum: "c8cd7b3e2514a0d94587a1bc50f184a48307af1e9acf6f89f55dd46b05c029a5"
+            checksum: "d5d901fd02439e1e3a0e71a57328ddfe807500caded1aaa1fae206b9e3e45936"
         )
     ]
 )
